6730e12cf84b8ad17610d541fba8512ee1dea743
Peter Zhou <xfzhou1990@gmail.com>
2019-12-09 15:34:45 -0800

    H2 initialization

---
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.xfz.ppmtool.repositories;
+
+import com.xfz.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
