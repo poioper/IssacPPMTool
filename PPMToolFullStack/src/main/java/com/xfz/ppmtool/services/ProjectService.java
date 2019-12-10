18fa634f3e8853d79993d7bbfee9789c4cf8d4a5
Peter Zhou <xfzhou1990@gmail.com>
2019-12-09 17:10:42 -0800

    Persist project entity using post man

--- PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectService.java
@@ -1,5 +1,6 @@
 package com.xfz.ppmtool.services;
 
+import com.xfz.ppmtool.domain.Project;
 import com.xfz.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class ProjectService {
 
     @Autowired
     private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
 }
