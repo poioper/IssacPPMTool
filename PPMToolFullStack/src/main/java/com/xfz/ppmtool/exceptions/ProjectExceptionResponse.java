2141d100017ce241f38b9242043448973f43e70e
Peter Zhou <xfzhou1990@gmail.com>
2019-12-10 15:29:34 -0800

    Object validation & Custom Exceptions for Unique Project Identifiers

---
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/exceptions/ProjectExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.xfz.ppmtool.exceptions;
+
+public class ProjectExceptionResponse {
+
+    private String projectIdentifier;
+
+    public ProjectExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
--- PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.xfz.ppmtool.services;
 
 import com.xfz.ppmtool.domain.Project;
+import com.xfz.ppmtool.exceptions.ProjectIdException;
 import com.xfz.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+ "'already exists");
+        }
     }
 }
