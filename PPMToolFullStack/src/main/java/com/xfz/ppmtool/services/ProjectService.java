631bea72ad9abfd9aefae4f7753db90a1bd8270d
Peter Zhou <xfzhou1990@gmail.com>
2019-12-11 20:45:16 -0800

    react setup

--- PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectService.java
@@ -35,4 +35,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. This project does not exists");
+        }
+
+        projectRepository.delete(project);
+    }
 }
