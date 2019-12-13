e24d4b7489608a201b9a6d0d3024a45ee0d1fd4d
Peter Zhou <xfzhou1990@gmail.com>
2019-12-12 20:13:36 -0800

    Style dashboard, navbar, projectItem

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,10 +5,21 @@ import Header from "./Layout/Header";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <h1>Welcome to the Dashboard</h1>
-        <Projectitem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <Projectitem />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
