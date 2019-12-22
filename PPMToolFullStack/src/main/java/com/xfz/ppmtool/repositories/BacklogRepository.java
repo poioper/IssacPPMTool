6c0f59ce0b45a70daa5df774d4be231ac2e330c3
Peter Zhou <xfzhou1990@gmail.com>
2019-12-21 19:00:35 -0800

    ProjectTask, backlog entities and relationship between project and backlog

---
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package com.xfz.ppmtool.repositories;
+
+import com.xfz.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
---
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.xfz.ppmtool.repositories;
+
+import com.xfz.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
