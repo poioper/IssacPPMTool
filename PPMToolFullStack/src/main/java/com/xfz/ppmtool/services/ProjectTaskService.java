46fd208615c9429ef6e26bad27709b7b845870da
Peter Zhou <xfzhou1990@gmail.com>
2019-12-25 17:53:35 -0800

    Add Project Tasks React

--- PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/xfz/ppmtool/services/ProjectTaskService.java
@@ -40,7 +40,7 @@ public class ProjectTaskService {
             projectTask.setProjectIdentifier(projectIdentifier);
 
             //INITIAL priority when priority null
-            if(projectTask.getPriority() == null) {
+            if(projectTask.getPriority()==0||projectTask.getPriority()==null) {
                 projectTask.setPriority(3);
             }
 
